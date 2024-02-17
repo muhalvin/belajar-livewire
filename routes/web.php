1ff5bdeb8920d323bbf464cc6fc7900c7761fecf
Muhammad Alvin <muhammadalvin888@gmail.com>
2024-02-17 14:37:09 +0700

    first commits

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/home', \App\Livewire\Home::class)->name('home');
+
+Route::get('/about', \App\Livewire\About::class)->name('about');
+
+Route::get('/contact', \App\Livewire\Contact::class)->name('contact');
+
+Route::get('/posts', \App\Livewire\Posts\Index::class)->name('posts.index');
